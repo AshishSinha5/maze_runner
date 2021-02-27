93a766987143929609b6c09130518a82fd1aee3a
AshishSinha5 <ashish97.kumar@gmail.com>
2021-02-27 11:29:00 +0530

    Added outputs and args

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
 import numpy as np
+import matplotlib.pyplot as plt
 import argparse
+from play import play
 
 
 def get_maze(maze_file):
@@ -12,9 +13,41 @@ def get_maze(maze_file):
     return maze
 
 
-
 if __name__ == "__main__":
-    maze_file = 'data/maze1.txt'
-    l = get_maze(maze_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file_path", help='FIle path to maze', default='data/maze1.txt', type=str)
+    parser.add_argument("-e", "--epsilon", help="Epsilon value", default=0.1, type=float)
+    parser.add_argument("-a", "--alpha", help='Alpha value fro TD-SARSA algorithm', default='0.5', type=float)
+    parser.add_argument("-g", "--gamma", help="Gamma value for TD-SARSA algorithm", default=0.9, type=float)
+    parser.add_argument("-n", "--num_trials", help="Number of trials/episodes", default=50000, type=int)
+    args = parser.parse_args()
+    maze_file =args.file_path
+    eps = args.epsilon
+    alpha = args.alpha
+    gamma = args.gamma
+    n_trials = args.num_trials
 
-    print(l)
+    maze = get_maze(maze_file)
+    print(maze.shape)
+    print(maze)
+
+    agent = play(maze, alpha=alpha, gamma=gamma, eps=eps, n_trials=n_trials)
+    camera, rewards, prop_succ = agent.td_sarsa()
+
+    animation = camera.animate()
+    animation.save('outputs/{}.gif'.format(maze_file[5:-4]))
+    plt.close()
+
+    plt.title("Rewards v/s Episodes")
+    plt.plot(np.arange(len(rewards)), rewards)
+    plt.xlabel('Episodes')
+    plt.ylabel('Rewards')
+    plt.savefig('plots/{}_rewards.png'.format(maze_file[5:-4]))
+    plt.close()
+
+    plt.title("Proportion of success v/s Number of trials")
+    plt.plot(np.arange(len(prop_succ)), prop_succ)
+    plt.xlabel('Episodes')
+    plt.ylabel('Proportion of Success')
+    plt.savefig('plots/{}_prop_succ.png'.format(maze_file[5:-4]))
+    plt.close()
